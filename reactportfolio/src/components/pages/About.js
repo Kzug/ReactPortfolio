7a4abc1e084da8c21356f0b2def40bbd521ee08a
Kiersten Zug <99204084+Kzug@users.noreply.github.com>
2022-07-25 14:13:23 -0700

    adding icons

--- reactportfolio/src/components/pages/About.js
+++ reactportfolio/src/components/pages/About.js
@@ -1,12 +1,67 @@
-import React from 'react';
+import React from "react";
+import profilepic from "../../assets/PXL_20220614_180601661.PORTRAIT_2.jpg";
+import "../../styles/About.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faFilePdf } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../../assets/ResumeKierstenZug.pdf";
+
+library.add(faLinkedin, faGithub, faEnvelope, faFilePdf);
 
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
-      <p>
-      Hi! I'm Kiersten. I'm a web developer excited to make a difference in the world.
-      </p>
+      <section id="about-me">
+        <div className="personal-logo">Kiersten Zug</div>
+
+        <div className="flex flex-1">
+          <div className="about-me-info row">
+            <div className="about-me-info-container">
+              <figure className="about-me-picture-mask">
+                <img
+                  src={profilepic}
+                  className="about-me-picture"
+                  alt="profile pic"
+                />
+              </figure>
+              <h1 className="about-me-info-title">
+                Hi! I'm <span className="text-bluegreen">Kiersten.</span>
+              </h1>
+              <p className="about-me-info-para">
+                I'm a<strong className="text-bluegreen"> web developer</strong>{" "}
+                excited to make a difference in the world.
+              </p>
+
+              <div className="about-me-links">
+                <a
+                  href="https://www.linkedin.com/in/kiersten-zug-124276b8/"
+                  className="about-me-link"
+                >
+                  <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+                </a>
+                <a href="https://github.com/Kzug" className="about-me-link">
+                  <FontAwesomeIcon icon="fa-brands fa-github" />
+                </a>
+                <a
+                  href="mailto:kierstenzug@gmail.com"
+                  className="about-me-link"
+                >
+                  <FontAwesomeIcon icon="fa-solid fa-envelope" />
+                </a>
+                <a
+                  href="../../assets/ResumeKierstenZug.pdf"
+                  download
+                  target="_blank"
+                  className="about-me-link"
+                >
+                  <FontAwesomeIcon icon="fa-solid fa-file-pdf" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
--- reactportfolio/src/components/pages/MyProjects.js
+++ reactportfolio/src/components/pages/MyProjects.js
@@ -7,30 +7,120 @@ import weatherapp from "../../assets/weatherapp2.png";
 
 export default function MyProjects() {
   return (
-    <div>
-      <h1>My Projects</h1>
-      <img
-        src={comclo}
-        className="project-img"
-        alt="community closet project"
-      />
-      <img src={bitcoin} className="project-img" alt="bitcoin project" />
-      <img src={weatherapp} className="project-img" alt="weatherapp project" />
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
-    </div>
+    <section id="projects">
+      <div className="container">
+        <div className="row">
+          <h1 className="section-title">
+            Here are some of my <span className="text-purple">projects:</span>
+          </h1>
+          <ul className="project-list">
+            <li className="project">
+              <div className="project-container">
+                <img
+                  src={comclo}
+                  className="project-img"
+                  alt="community closet project"
+                />
+                <div className="project-description">
+                  <h3 className="project-description-title">
+                    Community Closet (Group Project)
+                  </h3>
+                  <h4 className="project-description-sub-title">
+                    HTML, CSS, JavaScript, Node.JS, AWS S3, Multer, Heroku,
+                    MySQL, Sequelize ORM, Handlebars
+                  </h4>
+                  <p className="project-description-para">
+                    Community Closet is an application where users can post
+                    clothing items they wish to give away. Other users who wish
+                    to receive the items can comment on the listing to express
+                    interest and arrange pickup. Images are stored in AWS S3
+                    using Multer and the application is deployed on Heroku.
+                  </p>
+                  <div className="project-description-links">
+                    <a
+                      href="https://github.com/areitan/Community_Closet"
+                      target="_blank"
+                      className="project-description-link"
+                    >
+                      <span className="githubproj">View on Github</span>
+                      <i className="fab fa-github"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li className="project">
+              <div className="project-container">
+                <img
+                  src={bitcoin}
+                  className="project-img"
+                  alt="bitcoin project"
+                />
+                <div className="project-description">
+                  <h3 className="project-description-title">
+                    Bitcoin Tracker (Group Project)
+                  </h3>
+                  <h4 className="project-description-sub-title">
+                    HTML, CSS, JavaScript, API, Local storage, D3
+                  </h4>
+                  <p className="project-description-para">
+                    Bitcoin Tracker displays the bitcoin price in real time and
+                    the current number of Bitcoin Core releases. The bitcoin
+                    price for the last 30 days is displayed on a chart. The user
+                    can switch between a line chart or candlestick chart by
+                    clicking a button. This application uses an API from Github
+                    and an API from CoinGecko.
+                  </p>
+                  <div className="project-description-links">
+                    <a
+                      href="https://github.com/Phoenix-Staley/cryptocurrency_tracker"
+                      target="_blank"
+                      className="project-description-link"
+                    >
+                      <span className="githubproj">View on Github</span>
+                      <i className="fab fa-github"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li className="project">
+              <div className="project-container">
+                <img
+                  src={weatherapp}
+                  className="project-img"
+                  alt="weather project"
+                />
+                <div className="project-description">
+                  <h3 className="project-description-title">
+                    Weather Dashboard (Individual Project)
+                  </h3>
+                  <h4 className="project-description-sub-title">
+                    HTML, CSS, JavaScript, API, Local storage, Bootstrap
+                  </h4>
+                  <p className="project-description-para">
+                    A weather dashboard that displays the current weather of a
+                    city and the next five day forecast. It features dynamically
+                    updated HTML and CSS and uses local storage to store
+                    persistant data. It uses two third-party APIs provided by
+                    OpenWeather.
+                  </p>
+                  <div className="project-description-links">
+                    <a
+                      href="https://github.com/Kzug/WeatherApp"
+                      target="_blank"
+                      className="project-description-link"
+                    >
+                      <span className="githubproj">View on Github</span>
+                      <i className="fab fa-github"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
   );
 }
